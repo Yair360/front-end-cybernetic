cd6e1c9aec1861ce15fbe621458543656081d1a8
Yair Aru Acevedo <u202125984@upc.edu.pe>
2024-09-24 19:57:25 -0500

    feat: added routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,7 @@ const router = createRouter({
     routes: [
         {path: '/home', name: 'home', component: HomeComponent},
         {path: '/profile', name: 'profile', component: ProfileComponent},
+        // redireccion
         {path: '/', redirect: '/home'}
     ]
 });
